c6bf5bc941d75a5452cd29c125cee1038b68c8c3
kubra_arig <arigkubra@gmail.com>
2023-04-23 00:23:11 +0300

    design has been cerated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'big': ['Big Shoulders Display', 'cursive'],
+      'lexend': ['Lexend Deca', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'c-orange': 'hsl(31, 77%, 52%)',
+        'c-green': 'hsl(184, 100%, 22%)',
+        'c-dark-green': 'hsl(179, 100%, 13%)',
+        'c-gray': 'hsl(0, 0%, 95%)',
+        'c-light-gray': 'hsla(0, 0%, 100%, 0.75)',
+      },
+    },
   },
   plugins: [],
 }
